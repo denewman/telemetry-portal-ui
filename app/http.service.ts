da11fb49dcb908b79e5762a7f1a09e9f4b8032e8
Taison.Eady <taison.eady@gmail.com>
2016-09-28 15:28:37 +1000

    changed to cloud ip in httpservice

--- app/http.service.ts
+++ app/http.service.ts
@@ -18,7 +18,7 @@ import { Observable } from 'rxjs/Observable';
 export class HttpService {
     constructor (private http: Http) {}
 
-    private url = "http://localhost:3000/flask"; //TODO: Change this address to production server.
+    private url = "http://203.36.5.115:3000/flask"; //TODO: Change this address to production server.
 
     private subscriptionUrl = this.url + '/subscription';
     private policyGroupUrl = this.url + '/policyGroup';
