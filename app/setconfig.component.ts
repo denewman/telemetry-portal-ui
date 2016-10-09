6c6a1b71a4acc233ead94dcca602f971400ab7f5
Devorah L Newman <denewman@cisco.com>
2016-10-08 22:17:29 -0700

    test dynamically changing port number based on config option in set defaults tab

--- app/setconfig.component.ts
+++ app/setconfig.component.ts
@@ -10,11 +10,13 @@ import { ConfigDataService } from './config-data.service';
 export class SetConfigComponent implements OnInit {
   configOptions = ['YDK', 'Netconfig/Yang', 'SSH'];
   disableSubmit: boolean = true;
+  configOption: string;
   routerPort: number;
 
   constructor(public configDataService: ConfigDataService) {}
 
   ngOnInit() {
+        this.configOption = this.configDataService.config.configOption;
         this.routerPort = this.configDataService.config.port;
   }
 
