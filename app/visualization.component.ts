8321306c118bfb150c2507c75dcd117fa9cfb5a8
Zhongyang Wang <s3471293@student.rmit.edu.au>
2016-10-17 12:56:35 +1100

    data visualization feature

--- app/visualization.component.ts
+++ app/visualization.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component,Output, ViewChild, EventEmitter} from '@angular/core';
+import {HttpService} from './http.service';
+
 
 @Component({
   selector: 'visualization',
   templateUrl: '../templates/visualization.component.tpl.html',
-  styleUrls: ['../css/visualization.component.css']
+  styleUrls: ['../css/visualization.component.css'],
+  providers: [HttpService]
 })
-export class VisualizationComponent { }
+export class VisualizationComponent { 
+  getData:string;
+  postData:string;
+  constructor(private httpService:HttpService){}
+  errorMsg:string;
+  image:string;
+  show: boolean = false;
+
+
+  onTestGet(){
+    this.show = !this.show;
+
+    this.httpService.dataVisualization()
+       .subscribe(
+        data=>this.image = JSON.stringify(data),
+        error=>this.errorMsg = error,
+        ()=>console.log("done")
+    );
+    //console.log("data" + this.getData);
+    //console.log("err" + this.errorMsg);
+  }
+
+  submit(queryName: string) {
+    if (!queryName) { return; }
+    this.httpService.addQuery(queryName)
+      .subscribe(
+        data=>this.getData=JSON.stringify(data),
+        error=>this.errorMsg = error,
+        ()=>console.log("done")
+            );
+
+    if (this.show == true){
+      this.show = !this.show;
+    }
+    //console.log("data" + this.getData);
+    //console.log("err" + this.errorMsg);
+  }
+
+
+
+}
